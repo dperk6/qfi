bbebe44f9c48cb34191980010d9b78b1d9ef1630
dperk6 <daniel@perklin.ca>
2020-11-23 00:56:36 -0500

    staking

---
+++ contracts/QPoolStakingFactory.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^ 0.6.6;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownabled.sol";
+import "./QPoolRewards/sol";
+
+contract QPoolStakingFactory is Ownable {
+    // immutables
+    address public rewardsToken;
+    uint public stakingRewardsGenesis;
+
+    // QFactory
+    address public qfactory;
+
+    // the staking tokens for which the rewards contract has been deployed
+    address[] public stakingTokens;
+
+    // info about rewards for a particular staking token
+    struct StakingRewardsInfo {
+        address stakingRewards;
+        uint rewardAmount;
+    }
+
+    // rewards info by staking token
+    mapping(address => StakingRewardsInfo) public stakingRewardsInfoByStakingToken;
+
+    constructor(
+        address _rewardsToken,
+        uint _stakingRewardsGenesis
+    ) Ownable() public {
+        require(_stakingRewardsGenesis >= block.timestamp, 'StakingRewardsFactory::constructor: genesis too soon');
+
+        rewardsToken = _rewardsToken;
+        stakingRewardsGenesis = _stakingRewardsGenesis;
+    }
+
+    ///// permissioned functions
+
+    // deploy a staking reward contract for the staking token, and store the reward amount
+    // the reward will be distributed to the staking reward contract no sooner than the genesis
+    function deploy(address stakingToken, uint rewardAmount) public onlyOwner {
+        StakingRewardsInfo storage info = stakingRewardsInfoByStakingToken[stakingToken];
+        require(info.stakingRewards == address(0), 'StakingRewardsFactory::deploy: already deployed');
+        require(qfactory.isPool(stakingToken), 'StakingRewardsFactory::deploy: not a QPool');
+        info.stakingRewards = address(new StakingRewards(address(this), rewardsToken, stakingToken));
+        info.rewardAmount = rewardAmount;
+        stakingTokens.push(stakingToken);
+    }
+
+    ///// permissionless functions
+
+    // call notifyRewardAmount for all staking tokens.
+    function notifyRewardAmounts() public {
+        require(stakingTokens.length > 0, 'StakingRewardsFactory::notifyRewardAmounts: called before any deploys');
+        for (uint i = 0; i < stakingTokens.length; i++) {
+            notifyRewardAmount(stakingTokens[i]);
+        }
+    }
+
+    // notify reward amount for an individual staking token.
+    // this is a fallback in case the notifyRewardAmounts costs too much gas to call for all contracts
+    function notifyRewardAmount(address stakingToken) public {
+        require(block.timestamp >= stakingRewardsGenesis, 'StakingRewardsFactory::notifyRewardAmount: not ready');
+
+        StakingRewardsInfo storage info = stakingRewardsInfoByStakingToken[stakingToken];
+        require(info.stakingRewards != address(0), 'StakingRewardsFactory::notifyRewardAmount: not deployed');
+
+        if (info.rewardAmount > 0) {
+            uint rewardAmount = info.rewardAmount;
+            info.rewardAmount = 0;
+
+            require(
+                IERC20(rewardsToken).transfer(info.stakingRewards, rewardAmount),
+                'StakingRewardsFactory::notifyRewardAmount: transfer failed'
+            );
+            StakingRewards(info.stakingRewards).notifyRewardAmount(rewardAmount);
+        }
+    }
+}
