61ef19227d635b0f35e1233aae3ac9c147b68dfb
dperk6 <daniel@perklin.ca>
2020-11-26 00:31:31 -0500

    differentiate private and public

--- contracts/QPoolFactory.sol
+++ contracts/QPoolFactory.sol
@@ -6,14 +6,24 @@ import "./QPool.sol";
 import "./QPoolPublic.sol";
 
 contract QPoolFactory {
-    address[] private addresses;
+    address[] private allPools;
+    address[] private privatePools;
+    address[] private publicPools;
     mapping(address => bool) private isPool;
 
     event PoolCreated(QPool pool);
     event PublicPoolCreated(QPoolPublic pool);
 
     function getPools() public view returns (address[] memory) {
-        return addresses;
+        return allPools;
+    }
+
+    function getPrivatePools() public view returns (address[] memory) {
+        return privatePools;
+    }
+
+    function getPublicPools() public view returns (address[] memory) {
+        return publicPools;
     }
 
     function checkPool(address _poolAddress) public view returns (bool) {
@@ -24,7 +34,8 @@ contract QPoolFactory {
     public returns (address) {
         QPool pool = new QPool(_tokens, _amounts, msg.sender);
         emit PoolCreated(pool);
-        addresses.push(address(pool));
+        allPools.push(address(pool));
+        privatePools.push(address(pool));
         isPool[address(pool)] = true;
         return address(pool);
     }
@@ -33,7 +44,8 @@ contract QPoolFactory {
     public returns (address) {
         QPoolPublic pool = new QPoolPublic(_name, _tokens, _amounts, msg.sender);
         emit PublicPoolCreated(pool);
-        addresses.push(address(pool));
+        allPools.push(address(pool));
+        publicPools.push(address(pool));
         isPool[address(pool)] = true;
     }
 }
